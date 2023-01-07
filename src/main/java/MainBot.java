b282d2f6fbc2155234e9a88338921d9025a2b49f
Learner <test@gmail.com>
2023-01-07 11:54:33 +0500

    added proxy

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -1,3 +1,6 @@
+import org.apache.http.HttpHost;
+import org.apache.http.client.config.RequestConfig;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
@@ -7,12 +10,19 @@ public class MainBot {
     public static void main(String[] args) {
         try {
             TelegramBotsApi telegramBotApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotApi.registerBot(new BizningBot());
+
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setProxy(new HttpHost("172.17.69.148" , 3128))
+                    .build();
+
+            DefaultBotOptions botOptions = new DefaultBotOptions();
+            botOptions.setRequestConfig(requestConfig);
+
+            telegramBotApi.registerBot(new BizningBot(botOptions));
         } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
